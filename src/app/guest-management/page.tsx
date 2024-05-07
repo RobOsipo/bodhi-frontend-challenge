61bc6385009ba2d3072af9843e9916212819558c
Robert <osipovitchr@gmail.com>
2024-05-07 10:57:06 -0400

    initialize using @create-next-app

---
+++ src/app/guest-management/page.tsx
@@ -0,0 +1,9 @@
+// internal imports
+import styles from "../page.module.css";
+
+// guest-management page
+export function GuestManagement() {
+  return <main className={styles.main}></main>;
+}
+
+export default GuestManagement;
---
+++ src/helpers/api-fetch.tsx
@@ -0,0 +1,29 @@
+// external imports
+import { encode } from "base-64";
+
+// credentials from email
+const userName = "test";
+const password = "user";
+
+// base-64 credentials for basic authentication
+const getAuthorization = (user: string, password: string) => {
+  const token = `${user}:${password}`;
+
+  const hash = encode(token);
+
+  return `Basic ${hash}`;
+};
+
+// fetch function for api calls
+export const apiFetch = async (url: string, method: string, body?: any) => {
+  const response = await fetch(url, {
+    method,
+    headers: {
+      Authorization: getAuthorization(userName, password),
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  return response.json();
+};
